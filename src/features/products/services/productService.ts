4881f1a102bc14f493994102730d8b91718d2977
alpaenf <alfaenf_@gmail.com>
2026-03-26 11:18:31 +0700

    first committt

--- src/features/products/services/productService.ts
+++ src/features/products/services/productService.ts
@@ -1,21 +1,27 @@
-import { createClient } from "@/lib/supabase/client";
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
 
 export const addProduct = async (name: string, price: number, stock: number) => {
-  const supabase = createClient();
+  const supabase = await createClient();
   const { data, error } = await supabase.from("products").insert([{ name, price, stock }]);
   if (error) throw error;
+  revalidatePath("/produk");
   return data;
 };
 
 export const updateProduct = async (id: string, name: string, price: number, stock: number) => {
-  const supabase = createClient();
+  const supabase = await createClient();
   const { data, error } = await supabase.from("products").update({ name, price, stock }).eq("id", id);
   if (error) throw error;
+  revalidatePath("/produk");
   return data;
 };
 
 export const deleteProduct = async (id: string) => {
-  const supabase = createClient();
+  const supabase = await createClient();
   const { error } = await supabase.from("products").delete().eq("id", id);
   if (error) throw error;
+  revalidatePath("/produk");
 };
